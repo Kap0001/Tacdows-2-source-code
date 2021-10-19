9d20e157d903fd6e248847e3c9d532b0039ec2a0
Kap0001 <92782071+Kap0001@users.noreply.github.com>
2021-10-19 01:01:36 -0700

    Add files via upload

---
+++ Software.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Tacdows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tacdows, our new OS.");
+            Console.ReadKey();
+
+            Console.WriteLine("System error");
+            Console.WriteLine("Repairing system.dll");
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.WriteLine("Failed to repair");
+            Console.ReadKey();
+
+            Console.WriteLine("Booting up...");
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.WriteLine("Welcome");
+            Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("Desktop")
+            Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("notepad)
+            Console.Readline();
+        }
+    }
+}
